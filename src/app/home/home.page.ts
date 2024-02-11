c837146fbb06067bdb9c7bd1238bf64a487fea9e
Christo Mabbs <christo.mabbs@gmail.com>
2024-02-12 01:11:48 +1030

    button updates

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -9,14 +9,60 @@ export class HomePage {
 
   state : number = 0;
 
+  noScreenX : number = 0;
+  noScreenY : number = 0;
+
+  noLeft : number = 10;
+  noTop : number = 0;
+
   constructor( ) {}
 
-  ngOnInit(): void {
-    
-    // Detect Mouse movements
-    document.addEventListener('mousemove', (event) => {
-      console.log(event);
-    });
+  ngOnInit(): void { }
+
+  ngAfterViewInit() {
+
+    setTimeout(() => {
+
+      // Fetch the x y center of the no button
+      let noButton = document.getElementById('no');
+      let noButtonRect = noButton!.getBoundingClientRect();
+      this.noScreenX = noButtonRect.x + (noButtonRect.width / 2);
+      this.noScreenY = noButtonRect.y + (noButtonRect.height / 2);
+
+      // Detect Mouse movements
+      document.addEventListener('mousemove', (event) => {
+
+        // Get the x and y position of the mouse
+        let mouseX = event.clientX;
+        let mouseY = event.clientY;
+
+        // Calculate the distance between the mouse and the no button
+        let distanceX = mouseX - this.noScreenX;
+        let distanceY = mouseY - this.noScreenY;
+
+        // If this distance is less than 100, move the noLeft to the opposite direction
+        if (Math.abs(distanceX) < 50) {
+          if (distanceX > 0) {
+            this.noLeft--;
+            this.noScreenX--;
+          } else {
+            this.noLeft++;
+            this.noScreenX++;
+          }
+        }
+        if (Math.abs(distanceY) < 50) {
+          if (distanceY > 0) {
+            this.noTop--;
+            this.noScreenY--;
+          } else {
+            this.noTop++;
+            this.noScreenY++;
+          }
+        }
+
+      });
+
+    }, 1000);
 
   }
 
